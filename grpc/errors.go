781631884fa43c00234cef19d7b10e7454cf5a9a
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-10-16 18:18:25 -0400

    minor: added ErrNilGRPCInterceptions

---
+++ grpc/errors.go
@@ -0,0 +1,9 @@
+package grpc
+
+import (
+	"errors"
+)
+
+var (
+	ErrNilGRPCInterceptions = errors.New("nil grpc interceptions")
+)
--- local/interfaces.go
+++ local/interfaces.go
@@ -1,9 +1,13 @@
 package local
 
+import (
+	goapikey "github.com/ralvarezdev/go-api-key"
+)
+
 type (
 	// BasicService is the basic service interface for API key validation
 	BasicService interface {
+		goapikey.BasicService
 		Load(path string) error
-		IsAPIKeyValid(apiKey string) bool
 	}
 )
